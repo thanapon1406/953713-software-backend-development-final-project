7eda41c2fd7b96e2a9525ee0b36dfe6b153aede9
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2026-03-13 13:11:19 +0700

    Add election routes to server configuration

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.routes";
 import morgan from "morgan";
 import adminRoutes from "./routes/admin.routes";
 import voteRoutes from "./routes/vote.routes";
+import electionRoutes from "./routes/election.routes";
 
 // Import Routes
 
@@ -32,6 +33,8 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/votes", voteRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/election", electionRoutes);
+
 
 // 5. Global Error Handling Middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
